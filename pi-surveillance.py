af71febdcd8f750776ee389513e21ba83b3ea9be
Even Brenna <evenbrenna@gmail.com>
2017-04-18 21:20:20 +0000

    Bugfixes

--- pi-surveillance.py
+++ pi-surveillance.py
@@ -57,9 +57,9 @@ for f in camera.capture_continuous(rawCapture, format = "bgr", use_video_port = 
     timestamp = datetime.datetime.now()
     motionDetected = False
     statusText = "Everything is quiet"
-
+    
     # Resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width = 500)
+    frame = cv2.resize(frame, (640, 480))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -80,7 +80,7 @@ for f in camera.capture_continuous(rawCapture, format = "bgr", use_video_port = 
     # to fill in holes, then find contours on thresholded image
     thresh = cv2.threshold(frameDelta, config["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
     thresh = cv2.dilate(thresh, None, iterations = 2)
-    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    (_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Loop over the contours
     for c in cnts:
