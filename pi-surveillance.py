91763f14f5caef40b3a04938fed3097b8e81f322
Even Brenna <evenbrenna@gmail.com>
2017-04-18 21:35:13 +0200

    Fix indents

--- pi-surveillance.py
+++ pi-surveillance.py
@@ -51,86 +51,83 @@ motionCounter = 0
 
 # capture frames from the camera
 for f in camera.capture_continuous(rawCapture, format = "bgr", use_video_port = True):
-	# Grab the raw NumPy array representing the image and
-	# initialize the timestamp and status text
-	frame = f.array
-	timestamp = datetime.datetime.now()
+    # Grab the raw NumPy array representing the image and
+    # initialize the timestamp and status text
+    frame = f.array
+    timestamp = datetime.datetime.now()
     motionDetected = False
     statusText = "Everything is quiet"
 
-	# Resize the frame, convert it to grayscale, and blur it
-	frame = imutils.resize(frame, width = 500)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    # Resize the frame, convert it to grayscale, and blur it
+    frame = imutils.resize(frame, width = 500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-	# If the average frame is None, initialize it
-	if avg is None:
-		print "[INFO] starting background model..."
-		avg = gray.copy().astype("float")
-		rawCapture.truncate(0)
-		continue
+    # If the average frame is None, initialize it
+    if avg is None:
+        print "[INFO] starting background model..."
+        avg = gray.copy().astype("float")
+        rawCapture.truncate(0)
+        continue
 
-	# Accumulate the weighted average between the current frame and
-	# previous frames, then compute the difference between the current
-	# frame and running average
-	cv2.accumulateWeighted(gray, avg, 0.5)
-	frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+    # Accumulate the weighted average between the current frame and
+    # previous frames, then compute the difference between the current
+    # frame and running average
+    cv2.accumulateWeighted(gray, avg, 0.5)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 
     # Threshold the delta image, dilate the thresholded image
     # to fill in holes, then find contours on thresholded image
-	thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
-	thresh = cv2.dilate(thresh, None, iterations = 2)
-	(cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations = 2)
+    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-	# Loop over the contours
-	for c in cnts:
-		# If the contour is too small, ignore it
-		if cv2.contourArea(c) < conf["min_area"]:
-			continue
+    # Loop over the contours
+    for c in cnts:
+        # If the contour is large enough then compute the bounding
+        # box for the contour, draw it on the frame, and update the text
+        if cv2.contourArea(c) > conf["min_area"]:
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            text = "Motion detected!"
+            motionDetected = True
 
-		# Compute the bounding box for the contour,
-		# draw it on the frame, and update the text
-		(x, y, w, h) = cv2.boundingRect(c)
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		text = "Motion detected!"
-        motionDetected = True
-
-	# Draw the text and timestamp on the frame
-	ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-	cv2.putText(frame, "Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-	cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+    # Draw the text and timestamp on the frame
+    ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+    cv2.putText(frame, "Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
     # check to see if the room is occupied
-	if motionDetected:
-		# check to see if enough time has passed between uploads
-		if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
-			motionCounter += 1
+    if motionDetected:
+        # check to see if enough time has passed between uploads
+        if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
+            motionCounter += 1
 
-			# Check if the number of frames with consistent motion is high enough
-			if motionCounter >= conf["min_motion_frames"]:
-				if conf["use_dropbox"]:
-					t = TempImage()
-					cv2.imwrite(t.path, frame)
-					print "[UPLOAD] {}".format(ts)
-					path = "{base_path}/{timestamp}.jpg".format(
-						base_path = conf["dropbox_base_path"], timestamp = ts)
-					dropbox.put_file(path, open(t.path, "rb"))
-					t.cleanup()
+            # Check if the number of frames with consistent motion is high enough
+            if motionCounter >= conf["min_motion_frames"]:
+                if conf["use_dropbox"]:
+                    t = TempImage()
+                    cv2.imwrite(t.path, frame)
+                    print "[UPLOAD] {}".format(ts)
+                    path = "{base_path}/{timestamp}.jpg".format(
+                        base_path = conf["dropbox_base_path"], timestamp = ts)
+                    dropbox.put_file(path, open(t.path, "rb"))
+                    t.cleanup()
 
-				# update the last uploaded timestamp and reset the motion counter
-				lastUploaded = timestamp
-				motionCounter = 0
-	else:
-		motionCounter = 0
+                # update the last uploaded timestamp and reset the motion counter
+                lastUploaded = timestamp
+                motionCounter = 0
+    else:
+        motionCounter = 0
 
     # Check if the frames should be displayed to screen
-	if conf["show_video"]:
-		cv2.imshow("Security Feed", frame)
-		key = cv2.waitKey(1) & 0xFF
+    if conf["show_video"]:
+        cv2.imshow("Security Feed", frame)
+        key = cv2.waitKey(1) & 0xFF
 
-		# If the `q` key is pressed, break from the lop
-		if key == ord("q"):
-			break
+        # If the `q` key is pressed, break from the lop
+        if key == ord("q"):
+            break
 
-	# Clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
+    # Clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
